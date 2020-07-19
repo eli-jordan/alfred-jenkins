fc7aab557d45494eb8e52191001868bad6cacf52
Eli Jordan <elias.jordan@tapad.com>
2020-07-19 02:57:40 +0200

    get local development linking task to work

--- project/AlfredWorkflowPlugin.scala
+++ project/AlfredWorkflowPlugin.scala
@@ -5,66 +5,67 @@ import com.dd.plist.{NSArray, NSDictionary, NSString, PropertyListParser}
 import scala.sys.process.{Process => SProcess}
 
 /**
- * An sbt plugin for packaging alfred workflows
- */
+  * An sbt plugin for packaging alfred workflows
+  */
 object AlfredWorkflowPlugin extends AutoPlugin {
 
   object autoImport {
-    /**
-     * This is the name of the workflow. The value will be injected into the `info.plist` file
-     * and will also be the name of the generated .alfredworkflow artifact.
-     */
-    val alfredWorkflowName      = settingKey[String]("The name of the workflow")
 
     /**
-     * The version of the workflow. This will be injected into the `info.plist` file.
-     *
-     * Defaults to the value of the `version` setting
-     */
-    val alfredWorkflowVersion   = settingKey[String]("The workflow version number")
+      * This is the name of the workflow. The value will be injected into the `info.plist` file
+      * and will also be the name of the generated .alfredworkflow artifact.
+      */
+    val alfredWorkflowName = settingKey[String]("The name of the workflow")
 
     /**
-     * The directory that holds all static files that should be copied into the published workflow.
-     *
-     * - There MUST be an `info.plist` file describing the workflow in this directory.
-     * - If there is a readme.txt file, it will be copied into the workflow readme.
-     *
-     * Defaults to baseDirectory.value / "alfred"
-     */
-    val alfredWorkflowDir       = settingKey[File]("The directory where workflow files are stored")
+      * The version of the workflow. This will be injected into the `info.plist` file.
+      *
+      * Defaults to the value of the `version` setting
+      */
+    val alfredWorkflowVersion = settingKey[String]("The workflow version number")
 
     /**
-     * Workflow variables that should be injected into the `info.plist` prior to packaging.
-     */
+      * The directory that holds all static files that should be copied into the published workflow.
+      *
+      * - There MUST be an `info.plist` file describing the workflow in this directory.
+      * - If there is a readme.txt file, it will be copied into the workflow readme.
+      *
+      * Defaults to baseDirectory.value / "alfred"
+      */
+    val alfredWorkflowDir = settingKey[File]("The directory where workflow files are stored")
+
+    /**
+      * Workflow variables that should be injected into the `info.plist` prior to packaging.
+      */
     val alfredWorkflowVariables = settingKey[Map[String, String]]("Workflow variables to inject prior to publishing")
 
     /**
-     * Additional files that are not in [[alfredWorkflowDir]] that should be packaged with the workflow.
-     * This is generally used to copy build artifacts (such as an executable) into the workflow.
-     */
+      * Additional files that are not in [[alfredWorkflowDir]] that should be packaged with the workflow.
+      * This is generally used to copy build artifacts (such as an executable) into the workflow.
+      */
     val alfredWorkflowExtraFiles =
       taskKey[Seq[File]]("The list directories or files to include in the published workflow")
 
     /**
-     * Stage all files from [[alfredWorkflowDir]] and [[alfredWorkflowExtraFiles]] along with the
-     * processed `info.plist` into a directory, that just needed to be zipped up.
-     */
-    val alfredWorkflowStage   = taskKey[File]("Stage all files into the target directory")
+      * Stage all files from [[alfredWorkflowDir]] and [[alfredWorkflowExtraFiles]] along with the
+      * processed `info.plist` into a directory, that just needed to be zipped up.
+      */
+    val alfredWorkflowStage = taskKey[File]("Stage all files into the target directory")
 
     /**
-     * Zip a staged workflow directory
-     */
+      * Zip a staged workflow directory
+      */
     val alfredWorkflowPackage = taskKey[File]("Stage and package the workflow")
   }
 
   import autoImport._
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
-    Zero / alfredWorkflowDir := baseDirectory.value / "alfred",
-    Zero / alfredWorkflowVersion := version.value,
-    Zero / alfredWorkflowExtraFiles := Seq.empty,
-    Zero / alfredWorkflowVariables := Map.empty,
-    Zero / alfredWorkflowStage := {
+    alfredWorkflowDir := baseDirectory.value / "alfred",
+    alfredWorkflowVersion := version.value,
+    alfredWorkflowExtraFiles := Seq.empty,
+    alfredWorkflowVariables := Map.empty,
+    alfredWorkflowStage := {
       val outDir = target.value / "alfred" / "stage"
       stageWorkflow(
         name = alfredWorkflowName.value,
@@ -76,10 +77,11 @@ object AlfredWorkflowPlugin extends AutoPlugin {
       )
       outDir
     },
-    Zero / alfredWorkflowPackage := {
+    alfredWorkflowPackage := {
       val outDir     = target.value / "alfred"
       val stagingDir = alfredWorkflowStage.value
-      packageWorkflow(stagingDir, alfredWorkflowName.value, outDir)
+      val name = alfredWorkflowName.value
+      packageWorkflow(stagingDir, name, outDir)
     }
   )
 
@@ -112,8 +114,8 @@ object AlfredWorkflowPlugin extends AutoPlugin {
 
   def packageWorkflow(stagingDir: File, name: String, outDir: File): File = {
     val workflowPackage = outDir / s"$name.alfredworkflow"
-    val code = SProcess(Seq("zip", "-r", workflowPackage.toString, "."), Some(stagingDir)).!
-    if(code != 0) sys.error("Failed to package alfred workflow")
+    val code            = SProcess(Seq("zip", "-r", workflowPackage.toString, "."), Some(stagingDir)).!
+    if (code != 0) sys.error("Failed to package alfred workflow")
     workflowPackage
   }
 
@@ -129,17 +131,15 @@ object AlfredWorkflowPlugin extends AutoPlugin {
     properties.put("name", new NSString(name))
     properties.put("version", new NSString(version))
 
-    if(readme.exists()) {
+    if (readme.exists()) {
       properties.put("readme", new NSString(IO.read(readme)))
     }
 
     // Remove variables that are excluded from export
     val plistVars = properties.get("variables").asInstanceOf[NSDictionary]
-    val nonExported = properties
-      .get("variablesdontexport")
-      .asInstanceOf[NSArray]
-      .getArray
+    val nonExported = Option(properties.get("variablesdontexport"))
       .toList
+      .flatMap(_.asInstanceOf[NSArray].getArray.toList)
       .map(_.asInstanceOf[NSString])
       .map(_.toString)
 
@@ -148,8 +148,9 @@ object AlfredWorkflowPlugin extends AutoPlugin {
     }
 
     println(s"Setting variables in info.plist: $variables")
-    variables.foreach { case (key, value) =>
-      plistVars.put(key, new NSString(value))
+    variables.foreach {
+      case (key, value) =>
+        plistVars.put(key, new NSString(value))
     }
 
     PropertyListParser.saveAsXML(properties, outDir / "info.plist")
